367c48bb0e6f2a2a215caae0556488da96bc41e4
Wesley <matthew282@msn.com>
2023-01-13 11:36:45 -0700

    add respond_to logic to #index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,5 +4,11 @@ class StaticPagesController < ApplicationController
   def index
     flickr = Flickr.new
     @photos = flickr.people.getPublicPhotos(:user_id => params[:user_id]) if params[:user_id]
+
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @photos }
+      format.json {render :json => @photos }
+    end
   end
 end
