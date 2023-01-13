f47b48c575d322d101ae39269b435eec3fa48f22
Wesley <matthew282@msn.com>
2023-01-13 15:52:45 -0700

    remove respond_to

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,11 +4,5 @@ class StaticPagesController < ApplicationController
   def index
     flickr = Flickr.new
     @photos = flickr.people.getPublicPhotos(:user_id => params[:user_id]) if params[:user_id]
-
-    respond_to do |format|
-      format.html
-      format.xml { render :xml => @photos }
-      format.json {render :json => @photos }
-    end
   end
 end
