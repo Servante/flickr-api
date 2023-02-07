cea160c1c621a94ce270ad15a1e13d982e23e938
Wesley <matthew282@msn.com>
2023-02-07 15:26:44 -0700

    add error handling Flickr::FailedResponse

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,13 @@
 class StaticPagesController < ApplicationController
   require 'flickr'
+rescue_from Flickr::FailedResponse, with: :unknown_user
 
   def index
     flickr = Flickr.new
     @photos = flickr.people.getPublicPhotos(:user_id => params[:user_id]) if params[:user_id]
   end
+
+  def unknown_user
+    render :index
+  end  
 end
